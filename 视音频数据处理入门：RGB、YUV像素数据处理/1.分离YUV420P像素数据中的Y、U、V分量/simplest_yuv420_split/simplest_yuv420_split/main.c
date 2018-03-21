eb2c72346cd786d8c5bde1441ec870fd5244e8a9
Icarus <anglemiku_v@163.com>
2018-03-21 11:24:15 +0800

    simplest_yuv420_split

--- 视音频数据处理入门：RGB、YUV像素数据处理/1.分离YUV420P像素数据中的Y、U、V分量/simplest_yuv420_split/simplest_yuv420_split/main.c
+++ 视音频数据处理入门：RGB、YUV像素数据处理/1.分离YUV420P像素数据中的Y、U、V分量/simplest_yuv420_split/simplest_yuv420_split/main.c
@@ -10,26 +10,39 @@
 
 //add
 #include <stdlib.h>
-
+#include <string.h>
 
 /**
- * Split Y, U, V planes in YUV420P file.
- * @param url  Location of Input YUV file.
- * @param w    Width of Input YUV file.
- * @param h    Height of Input YUV file.
- * @param num  Number of frames to process.
- *
+ Split Y, U, V planes in YUV420P file.
+ 
+ @param inputUrl Location of Input YUV file.
+ @param w Width of Input YUV file.
+ @param h Height of Input YUV file.
+ @param num Number of frames to process.
+ @param outUrl Location of outPut YUV file.
+ @return 1-successed, 0-failed
  */
-int simplest_yuv420_split(char *url, int w, int h,int num);
+int simplest_yuv420_split(char *inputUrl, int w, int h,int num, char *outUrl);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
     
-    char *url = "/Users/angle/Desktop/FFmpeg/FFmpeg_Leixiaohua/视音频数据处理入门：RGB、YUV像素数据处理/resources/lena_256x256_yuv420p.yuv";
-    
+
+    //资源文件路径，此地址要更换为自己的电脑路径
+    char *inUrl = "/Users/angle/Desktop/FFmpeg/FFmpeg_Leixiaohua/视音频数据处理入门：RGB、YUV像素数据处理/resources/lena_256x256_yuv420p.yuv";
+    //输出资源路径，此地址要更换为自己的电脑路径
+    char *outUrl = "/Users/angle/Desktop/FFmpeg/FFmpeg_Leixiaohua/视音频数据处理入门：RGB、YUV像素数据处理/1.分离YUV420P像素数据中的Y、U、V分量/results/";
+
+
     //Test
-    simplest_yuv420_split(url,256,256,1);
+    int result = simplest_yuv420_split(inUrl,256,256,1,outUrl);
+
+    if (result == 1) {
+        printf("Write file successed.\n");
+    }else {
+        printf("Write file failed.\n");
+    }
     
     return 0;
 }
--- 视音频数据处理入门：RGB、YUV像素数据处理/1.分离YUV420P像素数据中的Y、U、V分量/simplest_yuv420_split/simplest_yuv420_split/simplest_yuv420_split.c
+++ 视音频数据处理入门：RGB、YUV像素数据处理/1.分离YUV420P像素数据中的Y、U、V分量/simplest_yuv420_split/simplest_yuv420_split/simplest_yuv420_split.c
@@ -10,40 +10,77 @@
 
 //add
 #include <stdlib.h>
-
+#include <string.h>
 
 /**
- * Split Y, U, V planes in YUV420P file.
- * @param url  Location of Input YUV file.
- * @param w    Width of Input YUV file.
- * @param h    Height of Input YUV file.
- * @param num  Number of frames to process.
- *
+ Splicing string    exp: splicingString_f("123","456")  out:"123456"
+ 
+ @param string_1 string on the left
+ @param string_2 string on the right
+ @return new string
  */
-int simplest_yuv420_split(char *url, int w, int h,int num){
-    FILE *fp=fopen(url,"rb+");
-    FILE *fp1=fopen("output_420_y.y","wb+");
-    FILE *fp2=fopen("output_420_u.y","wb+");
-    FILE *fp3=fopen("output_420_v.y","wb+");
+char * splicingString_f (char *string_1, char *string_2) {
+    
+    size_t len = strlen(string_1) + strlen(string_2) + 1;
+    
+    char * p;
+    p = (char * ) malloc (len);
+    memset   ( p , 0 , len);
+    strcpy ( p, string_1);
+    strcat ( p, string_2);
+
+    return p;
+}
+
+/**
+ Split Y, U, V planes in YUV420P file.
+
+ @param inputUrl Location of Input YUV file.
+ @param w Width of Input YUV file.
+ @param h Height of Input YUV file.
+ @param num Number of frames to process.
+ @param outUrl Location of outPut YUV file.
+ @return 1-successed, 0-failed
+ */
+int simplest_yuv420_split(char *inputUrl, int w, int h,int num, char *outUrl){
+    
+    FILE *fp=fopen(inputUrl,"rb+");
+    
+    FILE *fp1=fopen(splicingString_f(outUrl, "output_420_y.y"),"wb+");
+    FILE *fp2=fopen(splicingString_f(outUrl, "output_420_u.y"),"wb+");
+    FILE *fp3=fopen(splicingString_f(outUrl, "output_420_v.y"),"wb+");
     
     unsigned char *pic=(unsigned char *)malloc(w*h*3/2);
     
-    for(int i=0;i<num;i++){
+    if (fp != NULL && fp1 != NULL && fp2 != NULL && fp3 != NULL) {
         
-        fread(pic,1,w*h*3/2,fp);
-        //Y
-        fwrite(pic,1,w*h,fp1);
-        //U
-        fwrite(pic+w*h,1,w*h/4,fp2);
-        //V
-        fwrite(pic+w*h*5/4,1,w*h/4,fp3);
+        for(int i=0;i<num;i++){
+            
+            fread(pic,1,w*h*3/2,fp);
+            //Y
+            fwrite(pic,1,w*h,fp1);
+            //U
+            fwrite(pic+w*h,1,w*h/4,fp2);
+            //V
+            fwrite(pic+w*h*5/4,1,w*h/4,fp3);
+        }
+        
+        free(pic);
+        fclose(fp);
+        fclose(fp1);
+        fclose(fp2);
+        fclose(fp3);
+        
+        fp = NULL;
+        fp1 = NULL;
+        fp2 = NULL;
+        fp3 = NULL;
+        
+        return 1;
+    }else {
+        return 0;
     }
-    
-    free(pic);
-    fclose(fp);
-    fclose(fp1);
-    fclose(fp2);
-    fclose(fp3);
-    
-    return 0;
 }
+
+
+
