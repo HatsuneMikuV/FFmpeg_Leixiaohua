6fc97c7d3c574d531f7d14f2e786cc4f8ede00bd
Icarus <anglemiku_v@163.com>
2018-03-27 18:34:46 +0800

    update

---
+++ 最简单的基于FFMPEG+SDL的视频播放器/README.md
@@ -0,0 +1,17 @@
+# FFmpeg_Leixiaohua
+向我们的雷神--雷霄骅，致敬
+
+
+#### 本篇的目的是将雷神的贡献，以mac+xcode+C的环境，将组合式项目Demo，单一实现，方便于学习。
+#### 如有任何问题，请邮件(anglemiku_v@163.com)联系或者QQ(479069761)联系，欢迎纠正！！！
+
+
+* 该程序可以打印出FFmpeg类库的基本信息，使用该程序通常可以验证FFmpeg是否正确的安装配置
+
+
+##### ffmpeg 如果未安装，请先安装
+
+* 安装方法
+    *  brew install ffmpeg
+    * ->Build Settings -> Library Search Paths  地址更换为自己电脑地址（报错的情况下）
+
---
+++ 最简单的基于FFMPEG+SDL的视频播放器/simplest_ffmpeg_player/simplest_ffmpeg_player/main.c
@@ -0,0 +1,221 @@
+//
+//  main.c
+//  simplest_ffmpeg_player
+//
+//  Created by angle on 27/03/2018.
+//  Copyright © 2018 angle. All rights reserved.
+//
+
+#include <stdio.h>
+
+
+
+/**
+ * 最简单的基于FFmpeg的视频播放器
+ * Simplest FFmpeg Player
+ *
+ * 本程序实现了视频文件的解码和显示（支持HEVC，H.264，MPEG2等）。
+ * 是最简单的FFmpeg视频解码方面的教程。
+ * 通过学习本例子可以了解FFmpeg的解码流程。
+ * This software is a simplest video player based on FFmpeg.
+ * Suitable for beginner of FFmpeg.
+ */
+
+
+#include <stdio.h>
+
+#include "avcodec.h"
+#include "avformat.h"
+#include "swscale.h"
+#include "imgutils.h"
+#include "SDL.h"
+
+
+//Refresh Event
+#define SFM_REFRESH_EVENT  (SDL_USEREVENT + 1)
+
+#define SFM_BREAK_EVENT  (SDL_USEREVENT + 2)
+
+int thread_exit=0;
+int thread_pause=0;
+
+int sfp_refresh_thread(void *opaque){
+    thread_exit=0;
+    thread_pause=0;
+    
+    while (!thread_exit) {
+        if(!thread_pause){
+            SDL_Event event;
+            event.type = SFM_REFRESH_EVENT;
+            SDL_PushEvent(&event);
+        }
+        SDL_Delay(40);
+    }
+    thread_exit=0;
+    thread_pause=0;
+    //Break
+    SDL_Event event;
+    event.type = SFM_BREAK_EVENT;
+    SDL_PushEvent(&event);
+    
+    return 0;
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    
+    
+    
+    AVFormatContext *pFormatCtx;
+    int             i, videoindex;
+    AVCodecContext  *pCodecCtx;
+    AVCodec         *pCodec;
+    AVFrame *pFrame,*pFrameYUV;
+    unsigned char *out_buffer;
+    AVPacket *packet;
+    int ret, got_picture;
+    
+    //------------SDL----------------
+    int screen_w,screen_h;
+    SDL_Window *screen;
+    SDL_Renderer* sdlRenderer;
+    SDL_Texture* sdlTexture;
+    SDL_Rect sdlRect;
+    SDL_Thread *video_tid;
+    SDL_Event event;
+    
+    struct SwsContext *img_convert_ctx;
+    
+    //char filepath[]="bigbuckbunny_480x272.h265";
+    char filepath[]= "/Users/angle/Desktop/FFmpeg/FFmpeg_Leixiaohua/最简单的基于FFMPEG+SDL的视频播放器/res/Titanic.ts";
+    
+    av_register_all();
+    avformat_network_init();
+    pFormatCtx = avformat_alloc_context();
+    
+    if(avformat_open_input(&pFormatCtx,filepath,NULL,NULL)!=0){
+        printf("Couldn't open input stream.\n");
+        return -1;
+    }
+    if(avformat_find_stream_info(pFormatCtx,NULL)<0){
+        printf("Couldn't find stream information.\n");
+        return -1;
+    }
+    videoindex=-1;
+    for(i=0; i<pFormatCtx->nb_streams; i++)
+        if(pFormatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO){
+            videoindex=i;
+            break;
+        }
+    if(videoindex==-1){
+        printf("Didn't find a video stream.\n");
+        return -1;
+    }
+    pCodecCtx=pFormatCtx->streams[videoindex]->codec;
+    pCodec=avcodec_find_decoder(pCodecCtx->codec_id);
+    if(pCodec==NULL){
+        printf("Codec not found.\n");
+        return -1;
+    }
+    if(avcodec_open2(pCodecCtx, pCodec,NULL)<0){
+        printf("Could not open codec.\n");
+        return -1;
+    }
+    pFrame=av_frame_alloc();
+    pFrameYUV=av_frame_alloc();
+    
+    out_buffer=(unsigned char *)av_malloc(av_image_get_buffer_size(AV_PIX_FMT_YUV420P,  pCodecCtx->width, pCodecCtx->height,1));
+    av_image_fill_arrays(pFrameYUV->data, pFrameYUV->linesize,out_buffer,
+                         AV_PIX_FMT_YUV420P,pCodecCtx->width, pCodecCtx->height,1);
+    
+    //Output Info-----------------------------
+    printf("---------------- File Information ---------------\n");
+    av_dump_format(pFormatCtx,0,filepath,0);
+    printf("-------------------------------------------------\n");
+    
+    img_convert_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt,
+                                     pCodecCtx->width, pCodecCtx->height, AV_PIX_FMT_YUV420P, SWS_BICUBIC, NULL, NULL, NULL);
+    
+    
+    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER)) {
+        printf( "Could not initialize SDL - %s\n", SDL_GetError());
+        return -1;
+    }
+    //SDL 2.0 Support for multiple windows
+    screen_w = pCodecCtx->width;
+    screen_h = pCodecCtx->height;
+    screen = SDL_CreateWindow("Simplest ffmpeg player's Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                              screen_w, screen_h,SDL_WINDOW_OPENGL);
+    
+    if(!screen) {
+        printf("SDL: could not create window - exiting:%s\n",SDL_GetError());
+        return -1;
+    }
+    sdlRenderer = SDL_CreateRenderer(screen, -1, 0);
+    //IYUV: Y + U + V  (3 planes)
+    //YV12: Y + V + U  (3 planes)
+    sdlTexture = SDL_CreateTexture(sdlRenderer, SDL_PIXELFORMAT_IYUV, SDL_TEXTUREACCESS_STREAMING,pCodecCtx->width,pCodecCtx->height);
+    
+    sdlRect.x=0;
+    sdlRect.y=0;
+    sdlRect.w=screen_w;
+    sdlRect.h=screen_h;
+    
+    packet=(AVPacket *)av_malloc(sizeof(AVPacket));
+    
+    video_tid = SDL_CreateThread(sfp_refresh_thread,NULL,NULL);
+    //------------SDL End------------
+    //Event Loop
+    
+    for (;;) {
+        //Wait
+        SDL_WaitEvent(&event);
+        if(event.type==SFM_REFRESH_EVENT){
+            while(1){
+                if(av_read_frame(pFormatCtx, packet)<0)
+                    thread_exit=1;
+                
+                if(packet->stream_index==videoindex)
+                    break;
+            }
+            ret = avcodec_decode_video2(pCodecCtx, pFrame, &got_picture, packet);
+            if(ret < 0){
+                printf("Decode Error.\n");
+                return -1;
+            }
+            if(got_picture){
+                sws_scale(img_convert_ctx, (const unsigned char* const*)pFrame->data, pFrame->linesize, 0, pCodecCtx->height, pFrameYUV->data, pFrameYUV->linesize);
+                //SDL---------------------------
+                SDL_UpdateTexture( sdlTexture, NULL, pFrameYUV->data[0], pFrameYUV->linesize[0] );
+                SDL_RenderClear( sdlRenderer );
+                //SDL_RenderCopy( sdlRenderer, sdlTexture, &sdlRect, &sdlRect );
+                SDL_RenderCopy( sdlRenderer, sdlTexture, NULL, NULL);
+                SDL_RenderPresent( sdlRenderer );
+                //SDL End-----------------------
+            }
+            av_packet_unref(packet);
+        }else if(event.type==SDL_KEYDOWN){
+            //Pause
+            if(event.key.keysym.sym==SDLK_SPACE)
+                thread_pause=!thread_pause;
+        }else if(event.type==SDL_QUIT){
+            thread_exit=1;
+        }else if(event.type==SFM_BREAK_EVENT){
+            break;
+        }
+        
+    }
+    
+    sws_freeContext(img_convert_ctx);
+    
+    SDL_Quit();
+    //--------------
+    av_frame_free(&pFrameYUV);
+    av_frame_free(&pFrame);
+    avcodec_close(pCodecCtx);
+    avformat_close_input(&pFormatCtx);
+    
+    
+    return 0;
+}
