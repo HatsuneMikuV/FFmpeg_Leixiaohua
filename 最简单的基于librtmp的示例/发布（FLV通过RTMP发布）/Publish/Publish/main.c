cadc0a1b1c404d55cec3d9c5a8b219c3bef558d6
Icarus <anglemiku_v@163.com>
2018-03-27 11:58:09 +0800

    update

--- 最简单的基于librtmp的示例/发布（FLV通过RTMP发布）/Publish/Publish/main.c
+++ 最简单的基于librtmp的示例/发布（FLV通过RTMP发布）/Publish/Publish/main.c
@@ -8,8 +8,25 @@
 
 #include <stdio.h>
 
+
+int publish_using_packet(char *intUrl, char *sendUrl);
+int publish_using_write(char *intUrl, char *sendUrl);
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    char *intUrl = "/Users/angle/Desktop/FFmpeg/FFmpeg_Leixiaohua/最简单的基于librtmp的示例/resources/cuc_ieschool.flv";
+    char *sendUrl = "rtmp://localhost/rtmplive/livestream";
+
+    int result = publish_using_packet(intUrl, sendUrl);
+    //int result = publish_using_write(intUrl, sendUrl);
+    
+    if (result == 0) {
+        printf("Send file successed.\n");
+    }else {
+        printf("Send file failed.\n");
+    }
+    
+    
     return 0;
 }
